7cbab7baa8c5cfdaa9db1c455acc76ceed1328c5
Clayrise <54859935+Clayrisee@users.noreply.github.com>
2021-01-16 21:29:17 +0700

    Stub Project Week-3

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:week3_stub_project/splash_page.dart';
+import 'const.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Weekly DSC",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        primaryColor: kPrimaryColor,
+      ),
+      home: SplashPage(),
+    );
+  }
+  //TODO 1: Tambahkan aplikasi ke firebase
+  //TODO 2: Tambahkan library firebase
+  //TODO 3: Tambahkan data ke firebase
+  //TODO 4: Ubah HomePage agar dapat menarik data dari firebase menggunakan stream builder query snapshot.
+  //TODO 5: Tambahkan page baru bernama add_page.dart
+  //TODO 6: Desain Page tersebut untuk pertemuan berikutnya
+}
